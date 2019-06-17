ef91dbf7f38d2eeb6583bae6eb413f06752dec73
Benjamin Geyer <brgeyer49@gmail.com>
2019-06-17 19:20:31 -0400

    graceful shutdowns

--- assignment-dockerfile/app.js
+++ assignment-dockerfile/app.js
@@ -39,3 +39,25 @@ process.on('unhandledRejection', (err) => {
 });
 
 init();
+
+process.on('SIGINT', function onSigint () {
+	console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+  shutdown();
+});
+
+// quit properly on docker stop
+process.on('SIGTERM', function onSigterm () {
+  console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+  shutdown();
+})
+
+// shut down server
+function shutdown() {
+  server.close(function onServerClosed (err) {
+    if (err) {
+      console.error(err);
+      process.exitCode = 1;
+		}
+		process.exit();
+  })
+}
